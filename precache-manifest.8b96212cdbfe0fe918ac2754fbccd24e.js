eee0d083d6052ae5a0eb8a8f8dd7150f8a86e9c3
sirse <artiom.tretjakovas2@gmail.com>
2020-02-02 23:33:41 +0200

    Updates

---
+++ precache-manifest.8b96212cdbfe0fe918ac2754fbccd24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7a380e6e97851d617430b9f255960d",
+    "url": "/words/index.html"
+  },
+  {
+    "revision": "0d2fb90d54b6f81d9282",
+    "url": "/words/static/css/main.4dc986c1.chunk.css"
+  },
+  {
+    "revision": "a67507190a94a839a97b",
+    "url": "/words/static/js/2.bf0139ba.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/words/static/js/2.bf0139ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d2fb90d54b6f81d9282",
+    "url": "/words/static/js/main.50d1cc77.chunk.js"
+  },
+  {
+    "revision": "61391619111bc0fa5c9f",
+    "url": "/words/static/js/runtime-main.4744439c.js"
+  }
+]);
