af28a16628a99a4e9d74db0f135f9b066e95afbd
sirse <artiom.tretjakovas2@gmail.com>
2020-02-02 20:27:02 +0200

    added design

---
+++ src/components/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./app.scss"
+
+export default class App extends React.Component {
+
+    public render(): React.ReactNode {
+        return (
+            <div className="app-wrapper">
+
+                <div className="app-box">
+
+                    <span className="word-description">
+                        Choose words group
+                    </span>
+
+                    <input />
+
+                </div>
+
+            </div>
+        );
+    }
+
+}
